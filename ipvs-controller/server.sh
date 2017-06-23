5859dbee1c8ee39e5be2da0aee8d561b32fef40a
Kimitoshi Takahshi <ktaka@ccmp.jp>
2017-06-24 01:16:21 +0900

    Enabled iptables & sysctl setting for ipvs to work properly.

---
+++ ipvs-controller/server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sysctl -w net.ipv4.ip_forward="1"
+sysctl -w net.ipv4.vs.conntrack="1"
+sysctl -w net.netfilter.nf_conntrack_max="20000000"
+#sysctl -w net.nf_conntrack_max="20000000"
+
+iptables -t mangle -A PREROUTING  -p tcp  --dport 8888 -j MARK --set-mark 1
+iptables -t nat -A POSTROUTING -m ipvs --vaddr 0.0.0.0/0 -j MASQUERADE
+
+touch /etc/keepalived/ipvs.d/dummy.conf
+
+exec /server $@
+
