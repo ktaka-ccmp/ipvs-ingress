9a0b422241f6020e68c776604c89a2bba863ea3b
Kimitoshi Takahshi <ktaka@ccmp.jp>
2018-09-27 22:30:28 +0900

    Kuruwa setting

--- exabgp/exabgp.sh
+++ exabgp/exabgp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cat << EOF  > bgp.conf
-neighbor ${PEER_IP1} {
+neighbor ${PEER_IP} {
  description "peer1";
  router-id ${POD_IP};
  local-address ${POD_IP};
@@ -13,17 +13,6 @@ neighbor ${PEER_IP1} {
         }
 }
 
-neighbor ${PEER_IP2} {
- description "peer2";
- router-id ${POD_IP};
- local-address ${POD_IP};
- local-as ${AS_NUM};
- peer-as ${AS_NUM};
- hold-time 1800;
-        static {
-                route ${PREFIX} next-hop ${NODE_IP};
-        }
-}
 EOF
 
 /sbin/ip netns exec node /sbin/ip route replace ${PREFIX} dev docker0
