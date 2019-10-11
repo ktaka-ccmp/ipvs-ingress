345e3f1b2f50b2c2234df4d092cc83888b39344d
Kimitoshi Takahshi <ktaka@ccmp.jp>
2019-10-11 10:41:31 +0900

    wip

--- xlbd-controller/xlbd.sh
+++ xlbd-controller/xlbd.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
 exec /xlbd-controller $@
+
+#echo $@ > aaaa
+#sleep 10000
+#exec /sbin/ip netns exec node /xlbd-controller $@
+#/XlbBinary/xlb -i eth0 -r ; /XlbBinary/xlb -i eth0 -v
+
 
 
